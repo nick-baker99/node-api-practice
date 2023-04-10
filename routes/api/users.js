0eb988a689da812cea7bcf87797fd659badd5e8e
= <nicholas3.baker@live.uwe.ac.uk>
2023-04-10 14:03:42 +0100

    moved users route to api folder

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const usersController = require('../controllers/usersController');
-const ROLES_LIST = require('../config/roles_list');
-const verifyRoles = require('../middleware/verifyRoles');
+const usersController = require('../../controllers/usersController');
+const ROLES_LIST = require('../../config/roles_list');
+const verifyRoles = require('../../middleware/verifyRoles');
 
 router.route('/').get(verifyRoles(ROLES_LIST.Admin), usersController.getAllUsers);
 
--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use('/refresh', require('./routes/refresh'));
 app.use('/logout', require('./routes/logout'));
 
 app.use(verifyJWT);
-app.use('/users', require('./routes/users'));
+app.use('/users', require('./routes/api/users'));
 app.use('/employees', require('./routes/api/employees'));
 
 app.all('*', (req, res) => {
